189c7076f6bb8b61c11f84296235cb0de93853fe
xiaozhi <caixianzhi@codemao.cn>
2019-03-22 17:10:23 +0800

    mybatis plus mbg

---
+++ src/main/java/cn/codemao/user/controller/UserController.kt
@@ -0,0 +1,18 @@
+package cn.codemao.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author xiaozhi
+ * @since 2019-03-22
+ */
+@RestController
+@RequestMapping("/user/user")
+class UserController
---
+++ src/main/java/cn/codemao/user/mapper/UserMapper.kt
@@ -0,0 +1,14 @@
+package cn.codemao.user.mapper;
+
+import cn.codemao.user.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author xiaozhi
+ * @since 2019-03-22
+ */
+interface UserMapper : BaseMapper<User>
